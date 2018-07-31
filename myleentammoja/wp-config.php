eedd56e8972bf5cdd56945ae29667ee80e9039ac
Frozky <Frozky@gmail.com>
2018-08-01 00:47:49 +0300

    config MTP

--- myleentammoja/wp-config.php
+++ myleentammoja/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'WP');
+define('DB_NAME', 'MTP');
 
 /** MySQL database username */
 define('DB_USER', 'root');
